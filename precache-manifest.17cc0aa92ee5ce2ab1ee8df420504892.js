6f176e1490f273d04be9d75728e900429394ba4a
Bruno Alcântara <brunoalcantarajc@gmail.com>
2020-07-03 12:11:03 -0300

    Updates

---
+++ precache-manifest.17cc0aa92ee5ce2ab1ee8df420504892.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b67c5231a2c8b014d306ccae7ac839",
+    "url": "/index.html"
+  },
+  {
+    "revision": "643347a171935ab68a28",
+    "url": "/static/css/main.1d72c37f.chunk.css"
+  },
+  {
+    "revision": "9fb3681c1777f7168839",
+    "url": "/static/js/2.7233e2f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7233e2f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "643347a171935ab68a28",
+    "url": "/static/js/main.4ab57e20.chunk.js"
+  },
+  {
+    "revision": "a998ce0e80f99f1d9ed2",
+    "url": "/static/js/runtime-main.94fb6542.js"
+  },
+  {
+    "revision": "e43552cfce39fbd31cd2479456c19f0b",
+    "url": "/static/media/logotipo.e43552cf.png"
+  }
+]);
